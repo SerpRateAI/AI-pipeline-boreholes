de9b1bcc07f189d60da54f0f38c58e46fcfdeeda
Hamed AMIRI <amiiri.hamed@gmail.com>
2025-01-23 17:10:02 +0100

    add the script to calculate SMDs from ROI core images

--- ROI_selection.py
+++ ROI_selection.py
@@ -8,6 +8,9 @@ with two exceptions with heights around 500 pixels (CS_5057_5_B_12_1_1 and CS_50
 
 This script slide a window of size "image_size" over the images with a stride, and chooses the region with maximum fracture fraction.
 
+Usage:
+python ROI_selection.py --path_imgs D:\Hamed\SerpAIpipeline\data\core_
+images_all --image_size 512 --stride 128 --path_output D:\Hamed\SerpAIpipeline\data\cropped_test
 """
 
 import os
@@ -31,11 +34,11 @@ def parse_args():
 def crop_ROIs():
     args = parse_args()
 
-    image_size = 512
+    # image_size = 512
     # stride_x = 256 # 50% overlap in vertical direction
     # stride_y = 200
-    stride = 128
-    width = 1005
+    # stride = 128
+    # width = 1005
     # y_start = (width-image_size)//2 # for image_size of 512, y_start = 245, y is horizontal (width), python convensions
     for folder in os.listdir(args.path_imgs):
     #     print(f'folder: {folder}')
@@ -50,7 +53,8 @@ def crop_ROIs():
             print('------------------------')
         # binarize the images
         img = np.where(img == img.max(), 1, 0).astype(np.uint8) # cause the maximum value (fracture pixel) is 128 instead of 1)
-        height = img.shape[0]
+        
+        height, width = img.shape
         max_x_start = height - args.image_size + 1 ## determining the lowest x_start in the image so a crop of 512 can be selected
         max_y_start = width - args.image_size + 1
         
@@ -58,8 +62,8 @@ def crop_ROIs():
         best_fracf = -1
         best_coords = (0, 0)
         best_roi_image = None
-        for x in range(0, max_x_start, stride):
-            for y in range(0, max_y_start, stride):
+        for x in range(0, max_x_start, args.stride):
+            for y in range(0, max_y_start, args.stride):
                 
                 ##extract the RIO
                 img_roi = img[x: x + args.image_size, y:y + args.image_size]
@@ -75,7 +79,7 @@ def crop_ROIs():
         ## save the best roi image
         if best_roi_image is not None:
             # save the ROI image in the same folder as the original segmented images
-            tifffile.imwrite(os.path.join(args.path_imgs, folder, f'{folder}_ROI_x{best_coords[0]}_y{best_coords[1]}.tif'), best_roi_image)
+            # tifffile.imwrite(os.path.join(args.path_imgs, folder, f'{folder}_ROI_x{best_coords[0]}_y{best_coords[1]}.tif'), best_roi_image)
             # save the ROI image in a separate output folder
             tifffile.imwrite(os.path.join(args.path_output, f'{folder}_ROI_x{best_coords[0]}_y{best_coords[1]}.tif'), best_roi_image)
 
--- calculate_smds.py
+++ calculate_smds.py
@@ -1,34 +1,43 @@
 """This script calculates the polytope functions for 2D imgages from a IODP expedition.
-The core images are insided a parent folder containing subfolders in which you can find segmented images.
+Before running this script, you should first crop ROI images of size 512 by 512 and put them in a folder.
+Imges should be binary (0 and 1) with 1 for your feature of interest (fractures).
+if you have images of different size, you should do some changes in cpp code and recompile it (Cpp_source-> Polytope->Sample_Pn_UU.cpp):
 
-Before running this script, make sure that you have compiled the cpp code for calculating smds of your desired size.
-In folder Cpp_source_512, it has been compiled for image size of 512 by 512."""
+#define MAXX 513 --> change this to your image_size + 1 (e.g., for image size of 256, it should be 257)
+#define Nt 256 --> change this to half of you image size (e.g., for image size of 256, it should be 128)
+
+Output:
+This script saves a dictionary for each image in the output folder specified by user.
+In each dictionary, the polytope functions (s2, p3, p4, ..L, f2, f3, f4, fL) are the keys and values are the probabilities at each distance r.
+The name of each dictionary shows the core image name.
+
+Usage:
+python calculate_smds.py --path_ROI_imgs D:\Hamed\SerpAIpipeline\data\cropped_ROIs --cpathPn Cpp_source_512\Cpp_source\Polytope --runtimePn C
+pp_source_512\runtime --outputPn Cpp_source_512\runtime\output --path_output D:\Hamed\SerpAIpipeline\smd_outputs_test
+
+
+"""
 
 import os
 import joblib
 import argparse
-import numpy as np
-import pandas as pd
-import random
-from src.SMDs import Microstructure, twoDCTimage2structure_mod, calculate_polytopes, calculate_smd_list
+from tqdm import tqdm
+from src.SMDs import calculate_polytopes, calculate_smd_list
 
 import tifffile
 
-SEED = 43
-random.seed(SEED)
-np.random.seed(SEED)
 
 ## parsing arguments
 def parse_args():
   """Parses arguments."""
   parser = argparse.ArgumentParser()
-  parser.add_argument('--path_input', required= True, type=str,
-                       help='Full path to parent folder containing all the core image folders. ')
+  parser.add_argument('--path_ROI_imgs', required= True, type=str,
+                       help='Path to the folder containing all the ROI images selected with ROI_selection.py.')
   # parser.add_argument('--path_cpp_code', type=str, required= True, help='Path to the folder containing compiled cpp codes for different image sizes')
-  
-  parser.add_argument('--cpathPn', type= str, required = True, help = 'path to polytope folder in cpp_source')
-  parser.add_argument('--runtimePn', type= str, required = True, help = 'path to runtime folder in cpp_source')
-  parser.add_argument('--outputPn', type= str, required = True, help = 'path to output folder in runtime')
+  parser.add_argument('--image_size', type = int, default= 512, help = 'The size of ROI images')
+  parser.add_argument('--cpathPn', type= str, help = 'path to polytope folder in cpp_source')
+  parser.add_argument('--runtimePn', type= str, help = 'path to runtime folder in cpp_source')
+  parser.add_argument('--outputPn', type= str, help = 'path to output folder in runtime')
 
   parser.add_argument('--path_output', type =str, help= 'Path to the output folder to save dictinary containing the SMDs')
   
@@ -36,66 +45,65 @@ def parse_args():
 
 def quantify_imgs():
     args = parse_args()
-    
-    img = tifffile.imread(os.path.join(args.path_input)).astype(np.uint8)
-    print(f'Image shape: {img.shape}')
-    poly_dict = {}
-    list_polytopes = [ 'p3h', 'p3v','p4','p6', 'L']
-    # list_polytopes = ['p3h','p4', 'L']
-    # base_poly_path = r'D:\Hamed\PSI\Results\compiled_poly_cpps'
 
-    min_img_size = min(img.shape[1], img.shape[2]) if img.ndim==3 else min(img.shape[0], img.shape[1])
 
-    # start_slice = 0
-
-    # n_sample: image size
-    par={'name':'polytopes','begx': 0, 'begy': 0, 'nsamp': min_img_size, 'edge_buffer': 0,
-        'equalisation': False, 'equal_method': 'adaptive', 'stretch_percentile': 2,
-        'clip_limit': 0.03, 'tvdnoise': False, 'tv_weight': 0.15, 'tv_eps': 2e-04,
-        'median_filter': False, 'median_filter_length': 3,
-        'thresholding_method': 'manual', 'thresholding_weight': 0.85, 'nbins': 256,
-        'make_figs': False, 'fig_res': 400, 'fig_path':'./Plots/'}
-    
-
-    # cpathPn = r'D:\Hamed\PoreEditGAN_github\cpp_poly\512\Cpp_source\Polytope'
-    # runtimePn = r'D:\Hamed\PoreEditGAN_github\cpp_poly\512\runtime/'
-    # outputPn = r'D:\Hamed\PoreEditGAN_github\cpp_poly\512\runtime\output/'
-
-    # cpathPn = os.path.join(args.path_cpp_code, str(min_img_size), 'Cpp_source\Polytope')
-    # runtimePn = os.path.join(args.path_cpp_code, str(min_img_size), 'runtime/')
-    # outputPn = os.path.join(args.path_cpp_code, str(min_img_size), 'runtime\output/')
-
+    # path to cpp folders, this works when the full path is provided, not reelative path
     cpathPn = args.cpathPn
     runtimePn = args.runtimePn + '/'
     outputPn = args.outputPn + '/'
 
+    # ##hard coded path for image size of 512 by 512 (path to the folders here in github folder)
+    # cpathPn = r'Cpp_source_512\Cpp_source\Polytope'
+    # runtimePn = r'Cpp_source_512\runtime/'
+    # outputPn = r'Cpp_source_512\runtime\output/'
 
 
-    print(f'cpathPn: {cpathPn}')
-    print(f'runtimePn: {runtimePn}')
-    print(f'outputPn: {outputPn}')
+    par={'name':'polytopes','begx': 0, 'begy': 0, 'nsamp': args.image_size, 'edge_buffer': 0,
+    'equalisation': False, 'equal_method': 'adaptive', 'stretch_percentile': 2,
+    'clip_limit': 0.03, 'tvdnoise': False, 'tv_weight': 0.15, 'tv_eps': 2e-04,
+    'median_filter': False, 'median_filter_length': 3,
+    'thresholding_method': 'manual', 'thresholding_weight': 0.85, 'nbins': 256,
+    'make_figs': False, 'fig_res': 400, 'fig_path':'./Plots/'}
 
+    list_polytopes = ['p2', 'p3h', 'p3v','p4','p6', 'L']
+    # list_polytopes = ['p2']
+    
 
-    for poly in list_polytopes:
-        
-        if poly == 'p2':
+    for filename in tqdm(os.listdir(args.path_ROI_imgs)):
+        if filename.endswith(".tif"):
+            # Remove the file extension
+            name_without_ext = os.path.splitext(filename)[0]  # e.g. CS_5057_5_B_9_4_1_ROI_x1152_y256
+
+            # Split at '_ROI_' and take the first part
+            parts = name_without_ext.split("_ROI_")
+            prefix = parts[0]  # e.g. CS_5057_5_B_9_4_1
+
+            print(prefix)
+            img = tifffile.imread(os.path.join(args.path_ROI_imgs, filename))
+    #         print(f'image shape = {img.shape}')
             
-            s2, f2 = calculate_smd_list(img)
-            poly_dict['s2'] = s2
-            poly_dict['f2'] = f2
-            print(f"S2 shape: {poly_dict['s2'][0].shape}")
+            # calculate smds
+            poly_dict = {}
+            for poly in list_polytopes:
+                
+                if poly == 'p2':
 
-        else:
-            poly1, poly2 = calculate_polytopes(img, par, outputPn, cpathPn, runtimePn, polytope = poly)
-            poly_dict[poly] = poly1
-            # we determine the name of poly stored in the dict as key: f3h, f3v, f4, f6, fL
-            poly2_name = poly.replace('p', 'f') if poly.startswith('p') else 'f' + poly
-            poly_dict[poly2_name] = poly2
-            print(f"{poly} shape: {poly_dict[poly][0].shape}")
-        print(f'polytope {poly} done!')
+                    s2, f2 = calculate_smd_list(img)
+                    poly_dict['s2'] = s2
+                    poly_dict['f2'] = f2
+    #                 print(f"S2 shape: {poly_dict['s2'].shape}")
+                    
+                else:
+                    poly1, poly2 = calculate_polytopes(img, par, outputPn, cpathPn, runtimePn, polytope = poly)
+                    poly_dict[poly] = poly1
+                    # we determine the name of poly stored in the dict as key: f3h, f3v, f4, f6, fL
+                    poly2_name = poly.replace('p', 'f') if poly.startswith('p') else 'f' + poly
+                    poly_dict[poly2_name] = poly2
+    #                 print(f"{poly} shape: {poly_dict[poly][0].shape}")
+    #                 print(f'polytope {poly} done!')
+                    
+            joblib.dump(poly_dict, os.path.join(args.path_output, f'{prefix}.pkl'))
 
-    joblib.dump(poly_dict, os.path.join(args.path_output, 'SMDs.pkl'))
-  
 
 if __name__=="__main__":
     quantify_imgs()
